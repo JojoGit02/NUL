86009b6706c75e2cc5eead4767d061b486b7c620
JojoGit02 <jonathan-06@orange.fr>
2019-12-17 11:49:04 +0100

    Ajout inscription

--- Connexion.php
+++ Connexion.php
@@ -13,8 +13,29 @@
          * Si Parametres correctes on indique que l'utilisateur qu'il est connecte 
          */
 
-         if (isset($_POST[Valider"]))
+         if (isset($_POST["Valider"]))
+         //On récupere les données brut 
          $Email = $_POST['ZoneEmail'];
          $MDP = $_POST['ZoneMdp'];
+         
+        // Paramétres de connexion
+         $host = 'localhost';
+         $user = 'user2';
+         $passwd = 'snir@snir2019';
+         $mabasse = 'biblio2';
 
+         //Connexion au SGBD MySQL
+         if ($conn = mysqli_connext($host,$user,$passwd,$mabasse))
+         {
+             //Nombre de ligne envoyé > 0
+             $Ligne = mysqli_num_rows($result);
+             if ($Ligne==1)
+             {
+                 //Extraction ligne envoyée par la requéte
+                $row = mysqli_fetch_assoc($result);
+                //Recuperation du mot de passe dans la ligne
+                $Mdp_Crypt_BD = $row['mdp'];
+                
+             }
+         }
         ?>
